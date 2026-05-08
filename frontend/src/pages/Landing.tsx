b45fb4150424b52e1fe3cf5219d051fb079c5635
dwyaneramos <riz3ndrr@gmail.com>
2026-05-09 00:45:08 +1200

    feat: changed width of get started button

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -3,7 +3,7 @@ import LandingAnimation from "@/components/LandingAnimation";
 export const Landing = () => {
   return (
     <div className="relative text-primary h-screen bg-bg flex flex-col justify-center overflow-hidden">
-      
+
       <div className="absolute inset-0 left-32 z-0">
         <LandingAnimation />
       </div>
@@ -15,7 +15,7 @@ export const Landing = () => {
           What your life <span className="italic">'kibi'</span>
         </h2>
 
-        <button className="bg-primary w-48 rounded-full my-3 text-bg text-3xl px-6 py-3">
+        <button className="bg-primary w-64 rounded-full my-3 text-bg text-3xl px-6 py-3">
           Get started
         </button>
       </div>
