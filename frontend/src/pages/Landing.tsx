63202ba7474967f6afc0885006a4940ca7d837ba
maxwmoir@gmail.com <maxwmoir@gmail.com>
2026-05-09 00:29:45 +1200

    Add animation to landing page

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,11 +1,26 @@
+import LandingAnimation from "@/components/LandingAnimation";
+
 export const Landing = () => {
   return (
-    <div className="text-primary h-full bg-bg flex flex-col pl-20 justify-center">
-      <h1 className="text-9xl">Kibi</h1>
-      <h2 className="text-6xl">What your life <span className="italic">'kibi'</span></h2>
-      <button className="bg-primary w-48 rounded-full my-3 text-bg text-3xl">Get started</button>
+    <div className="relative text-primary h-screen bg-bg flex flex-col justify-center overflow-hidden">
+      
+      <div className="absolute inset-0 left-32 z-0">
+        <LandingAnimation />
+      </div>
+
+      <div className="relative z-10 pl-20">
+        <h1 className="text-9xl">Kibi</h1>
+
+        <h2 className="text-6xl">
+          What your life <span className="italic">'kibi'</span>
+        </h2>
+
+        <button className="bg-primary w-48 rounded-full my-3 text-bg text-3xl px-6 py-3">
+          Get started
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Landing;
