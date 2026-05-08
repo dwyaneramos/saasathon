7043da03339d974621aa71d9efd7d4bbb9893381
slickramen <slickestramen@gmail.com>
2026-05-09 00:34:27 +1200

    chore: heartbeat endpoint and basic bruno test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express, {
 	type Request,
 	type Response,
 	type NextFunction,
 } from "express";
-import dotenv from "dotenv";
-dotenv.config();
-import userRoutes from "./routes/users.js";
+import apiRoutes from "./routes/index.js";
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-	res.json({ message: "Server is running with ESM and TypeScript!" });
+app.use("/api/v1", apiRoutes);
+
+app.get("/api/v1/health", (req: Request, res: Response) => {
+	res.json({ status: "ok", timestamp: new Date().toISOString() });
 });
 
 app.use((req: Request, res: Response) => {
@@ -25,8 +28,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 	res.status(500).json({ error: "Something went wrong!" });
 });
 
-app.use("/users", userRoutes);
-
 app.listen(PORT, () => {
-	console.log(`🚀 Server ready at: http://localhost:${PORT}`);
+	console.log(`Server ready at: http://localhost:${PORT}`);
 });
