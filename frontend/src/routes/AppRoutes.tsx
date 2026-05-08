339146f96264b402d751ecc769d3ce594279056e
dwyaneramos <riz3ndrr@gmail.com>
2026-05-08 23:22:44 +1200

    feat: fixed landing

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -4,12 +4,13 @@ import Graph from "../pages/Graph";
 import Login from "../pages/Login";
 import ViewProfile from "../pages/ViewProfile";
 import Navbar from "../components/Navbar";
+import Landing from "../pages/Landing";
 
 const AppRoutes = () => (
   <BrowserRouter>
     <Navbar />
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Landing />} />
       <Route path="/graph" element={<Graph />} />
       <Route path="/login" element={<Login />} />
       <Route path="/view-profile" element={<ViewProfile />} />
