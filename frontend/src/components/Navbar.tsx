551185abbba3fcfb3845b6ccd68eb1130987bbb7
slickramen <slickestramen@gmail.com>
2026-05-09 14:20:55 +1200

    chore: fix nav links

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,16 @@
 import { Link, NavLink } from "react-router-dom";
 import { Sparkles, Share2, LogIn, UserPlus } from "lucide-react";
 import kibiLogo from "../assets/kibi-logo.png";
+import { useAuth } from "@/context/AuthContext";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
 
 const Navbar = () => {
+	const { user, logout } = useAuth();
+
 	return (
 		<nav className="flex items-center justify-between px-8 py-4 mx-auto w-full relative z-50 text-zinc-900 border-b border-zinc-100 bg-white/80 backdrop-blur-md sticky top-0">
 			<div className="flex items-center justify-between max-w-7xl mx-auto w-full">
@@ -45,22 +53,73 @@ const Navbar = () => {
 					</div>
 
 					{/* AUTH ACTIONS */}
-					<div className="flex items-center gap-4">
-						<Link
-							to="/login"
-							className="flex items-center gap-1.5 text-zinc-500 hover:text-zinc-900 transition-colors"
-						>
-							<LogIn size={16} strokeWidth={2.25} />
-							Log in
-						</Link>
-						<Link
-							to="/register"
-							className="flex items-center gap-1.5 bg-zinc-900 text-white px-4 py-2 rounded-lg hover:bg-zinc-800 transition-all transform active:scale-95 shadow-lg shadow-zinc-200"
-						>
-							<UserPlus size={16} strokeWidth={2.25} />
-							Sign up
-						</Link>
-					</div>
+
+					{!user ? (
+						<>
+							<div className="flex items-center gap-4">
+								<Link
+									to="/login"
+									className="flex items-center gap-1.5 text-zinc-500 hover:text-zinc-900 transition-colors"
+								>
+									<LogIn size={16} strokeWidth={2.25} />
+									Log in
+								</Link>
+								<Link
+									to="/register"
+									className="flex items-center gap-1.5 bg-zinc-900 text-white px-4 py-2 rounded-lg hover:bg-zinc-800 transition-all transform active:scale-95 shadow-lg shadow-zinc-200"
+								>
+									<UserPlus size={16} strokeWidth={2.25} />
+									Sign up
+								</Link>
+							</div>
+						</>
+					) : (
+						<>
+							<Popover>
+								<PopoverTrigger asChild>
+									<img
+										className="w-8 h-8 cursor-pointer rounded-full border-1 border-zinc-400"
+										src="/default.png"
+										alt="User Profile"
+									/>
+								</PopoverTrigger>
+								<PopoverContent className="w-64 mr-3 mt-3">
+									<div className="space-y-4">
+										<div className="space-y-1">
+											<h4 className="text-sm font-medium">
+												First name
+											</h4>
+											<p className="text-sm text-muted-foreground">
+												{user.firstName}
+											</p>
+										</div>
+										<div className="space-y-1">
+											<h4 className="text-sm font-medium">
+												Last name
+											</h4>
+											<p className="text-sm text-muted-foreground">
+												{user.lastName}
+											</p>
+										</div>
+										<div className="space-y-1">
+											<h4 className="text-sm font-medium">
+												Email
+											</h4>
+											<p className="text-sm text-muted-foreground">
+												{user.email}
+											</p>
+										</div>
+										<button
+											onClick={logout}
+											className="w-full text-left text-sm text-red-500 hover:underline pt-2 border-t"
+										>
+											Log out
+										</button>
+									</div>
+								</PopoverContent>
+							</Popover>
+						</>
+					)}
 				</div>
 			</div>
 		</nav>
